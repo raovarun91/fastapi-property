9bef2b16f71f8ead2055617e0a455730ecd9c285
raovarun91 <30377012+raovarun91@users.noreply.github.com>
2022-12-26 21:26:39 +0400

    One path behind Authentication

--- Property/main.py
+++ Property/main.py
@@ -2,12 +2,14 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import property, user
+from .routers import property, user, authentication
 
 
 app =FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.include_router(authentication.router)
 app.include_router(property.router)
 app.include_router(user.router)
 
